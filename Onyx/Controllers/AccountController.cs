6f9a50b7b65ff45af6e7e6620f77313afcc3f30e
USER <USER@NITHEESH>
2024-04-23 12:37:04 +0400

    Procedure Fertching commented

--- Onyx/Controllers/AccountController.cs
+++ Onyx/Controllers/AccountController.cs
@@ -26,7 +26,7 @@ namespace Onyx.Controllers
         }
         public IActionResult Login()
         {
-            _commonService.GenerateModifiedSp();
+            //_commonService.GenerateModifiedSp();
             ViewBag.CompanyItems = _dbGatewayService.GetCompanies().Select(m => new SelectListItem
             {
                 Text = m.CoName,
