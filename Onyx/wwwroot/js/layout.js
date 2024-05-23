8575083aa74a54184a96b9d3fd22ddaf6cc25387
gines <gines@GINESH-PC>
2024-05-23 20:53:21 +0530

    resolve language set loading time

--- Onyx/wwwroot/js/layout.js
+++ Onyx/wwwroot/js/layout.js
@@ -2,9 +2,11 @@
 $("#language-dropdown .dropdown-item").on('click', function (e) {
     e.preventDefault();
     var lang = $(this).attr("data-value");
-    postAjax("/home/SetLanguage", { culture: lang }, function (response) {
-        window.location.reload();
-    });
+    $("#culture").val(lang);
+    $("#selectLanguage").submit();
+    //postAjax("/home/SetLanguage", { culture: lang }, function (response) {
+    //    window.location.reload();
+    //});
 })
 $("#user-company-dropdown").on('change', function (e) {
     postAjax("/home/UpdateClaim", { claimType: 'CompanyCd', claimValue: e.target.value }, function (response) {
